3839bd03a4a265cdbe8e1e2fb85b862ff3113edf
Grzegorz <85482129+TheBlackSurf@users.noreply.github.com>
2025-05-09 06:59:40 +0000

    update

--- https_domain_fix.sh
+++ https_domain_fix.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 FILE="/var/www/html/wp/wp-config.php"
+DIR="/var/www/html/wp"
 CODE="if (isset(\$_SERVER['HTTP_X_FORWARDED_PROTO']) && \$_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {\n    \$_SERVER['HTTPS'] = 'on';\n}"
 
 # Sprawdź, czy plik istnieje
@@ -12,17 +13,19 @@ fi
 # Sprawdź, czy kod już istnieje
 if grep -q "HTTP_X_FORWARDED_PROTO" "$FILE"; then
   echo "Kod już istnieje w $FILE."
-  exit 0
+else
+  # Dodaj kod po <?php
+  awk -v insert="$CODE" '
+    NR==1 && $0 ~ /<\?php/ {
+      print;
+      print insert;
+      next
+    }
+    { print }
+  ' "$FILE" > "${FILE}.tmp" && mv "${FILE}.tmp" "$FILE"
+  echo "Kod został dodany do $FILE."
 fi
 
-# Dodaj kod po <?php
-awk -v insert="$CODE" '
-  NR==1 && $0 ~ /<\?php/ {
-    print;
-    print insert;
-    next
-  }
-  { print }
-' "$FILE" > "${FILE}.tmp" && mv "${FILE}.tmp" "$FILE"
-
-echo "Kod został dodany do $FILE."
+# Zmień właściciela katalogu
+sudo chown -R www-data:www-data "$DIR"
+echo "Zmieniono właściciela katalogu $DIR na www-data:www-data."
